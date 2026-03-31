83e60d075b5f305e3e00ed1b4a46d4be1528af87
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-03-31 10:08:29 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -10,5 +10,13 @@ int main()
 	cin >> N;
 
 	// TODO
+	cout << n1 << " " << n2 << " ";
 	// TODO
+	for (int i = 0; i < N - 2; i++){
+		sum = n1 + n2;
+		cout << sum << " ";
+		n1 = n2;
+		n2 = sum;
+	}
+	cout << endl;
 }
